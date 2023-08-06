4df1b60c70d651b2f11ce9b1d825636d9d2b1631
James Alan Bush <jamesalanbush1972@gmail.com>
2023-08-06 15:35:17 -0400

    !!!!!

---
+++ ToneBarrier/RoutePickerView.h
@@ -0,0 +1,16 @@
+//
+//  RoutePickerView.h
+//  ToneBarrier
+//
+//  Created by Xcode Developer on 8/6/23.
+//
+
+#import <AVKit/AVKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RoutePickerView : AVRoutePickerView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ToneBarrier/RoutePickerView.m
@@ -0,0 +1,20 @@
+//
+//  RoutePickerView.m
+//  ToneBarrier
+//
+//  Created by Xcode Developer on 8/6/23.
+//
+
+#import "RoutePickerView.h"
+
+@implementation RoutePickerView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
